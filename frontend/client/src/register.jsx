9ef115a2c066b33fc05d791f1550d517ff5d05b9
menfistico <129977242+menfistico@users.noreply.github.com>
2023-07-11 15:57:12 -0600

    compare

--- frontend/client/src/register.jsx
+++ frontend/client/src/register.jsx
@@ -1,6 +1,12 @@
 export default function Register(){
     return(
-     <div>register screen here</div> 
+     <div className="bg-blue-50 h-screen">
+       <form className="w-64 mx-auto">
+         <input type="text" placeholder="username" className="block w-full"/>
+         <input type="password" placeholder="password" className="block w-full"/>
+         <button className="bg-blue-500 text-white block w-full">Register</button>
+       </form>
+     </div> 
     
     )
 }
