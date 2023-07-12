97f0e3c59c81cb18360db6c8a462be3ef95cf0f5
tonyxde <anthonybeitia2@gmail.com>
2023-07-12 06:29:37 -0600

    updated

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,19 +1,35 @@
 const express = require('express');
-const dotenv = require ('dotenv');
-const User = require('./models/User');
+const Mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const jwt = require ('jsonwebtoken');
+const cors = require('cors')
+const User = require('./models/User')
 
-dotenv.config();
-const app = express();
+dotenv.config(); 
+Mongoose.connect(process.env.MONGO_URL);
+const jwtSecret = process.env.JWR_secret;
 
-console.log(process.env.MONGO_URL);
-app.get('/test' , (req,res) =>{
-    res.json('test ok');
+const app = express()
+
+
+app.use (cors({
+  credentials:true,
+}));
+app.get('/test', (req,res) =>{
+    res.json('test ok');
 });
 
-app.post('/register',async(req,res)=>{
-const {usarname,password} =req.body;
-await User.create ({usarname,password});
+app.post('/register', async (req,res) =>{
+    const {username,password} = req.body;
+    const creasedUser = await User.create({username,password});
+    jwt.sign({userID:createdUser. id}, jwtSecret,(err,token) => {
+        if (err)throw err;
+        res.cookie('token', token).status(201).json ('ok');
+    });
+    });
+    
+    app.listen(4040);
+    
 
-});
 
-app.listen(4000);
+app.listen(4040);
