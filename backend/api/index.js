6695787d1b11ff3abc6b3658e1537e3a8b587d4e
tonyxde <anthonybeitia2@gmail.com>
2023-07-12 13:43:44 -0600

    login

--- backend/api/index.js
+++ backend/api/index.js
@@ -12,12 +12,20 @@ const jwtSecret = process.env.JWT_SECRET;
 const app = express()
 
 app.use(express.json())
-app.use (cors({
+app.use (cors({ 
  credentials:true,
  origin: process.env.CLIENT_URL,
 }));
 app.get('/test', (req,res) =>{
   res.json('test ok');
+});
+
+app.get('/profile', (req,res)=>   { 
+ const {token}=req.kookies;
+ jwt.verify(token, jwtSecret, {}, (err, userdeta)=>{ 
+  if (err) throw err;
+  res.json(userDate);  
+ });
 });
 
 app.post('/register', async (req,res) =>{
@@ -27,12 +35,13 @@ app.post('/register', async (req,res) =>{
       jwt.sign({userId: createdUser. id}, jwtSecret,(err,token) => {
       if (err) throw err;
         res.cookie('token', token).status(201).json ({
-           id: createdUser._id,   
+           id: createdUser._id,
+           username,   
         });
       });
     }
     catch (err) {
-      if (err) throw err;
+      console.log(err);
       res.status(500).json('error')
     }
     
--- frontend/client/src/UserContext.jsx
+++ frontend/client/src/UserContext.jsx
@@ -1,13 +1,16 @@
-import {createContext, useState } from "react";
+import {createContext, useEffect, useState } from "react";
 
 export const UserContext = createContext({});
 
-export function UserContextProvider({Children}){
-const[username, setUsername] = useState(null);
-const [id, setId] = useState(null);
+export function UserContextProvider({children}){
+    const[username, setUsername] = useState(null);
+    const [id, setId] = useState(null);
+    useEffect( () => {
+        
+    })
     return(
         <UserContext.Provider value={{username, setUsername, id, setId}}>
-            {Children}
+            {children}
         </UserContext.Provider>
     );
 } 
