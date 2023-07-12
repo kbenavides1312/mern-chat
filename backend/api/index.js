43765c49a9b906a405e18167cc9aca6dcdef30b9
tonyxde <anthonybeitia2@gmail.com>
2023-07-11 21:45:54 -0600

    usarname,passwor

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
-const dotenv= require ('dotenv');
+const dotenv = require ('dotenv');
+const User = require('./models/User');
 
 dotenv.config();
 const app = express();
+
 console.log(process.env.MONGO_URL);
 app.get('/test' , (req,res) =>{
     res.json('test ok');
 });
 
-app.post('/register',(req,res)=>{
+app.post('/register',async(req,res)=>{
+const {usarname,password} =req.body;
+await User.create ({usarname,password});
 
 });
 
