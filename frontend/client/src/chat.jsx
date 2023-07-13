0dd7142d9e71017c7dc31f04203861cffe4ff207
tonyxde <anthonybeitia2@gmail.com>
2023-07-13 12:09:30 -0600

    mensajes listo

--- frontend/client/src/chat.jsx
+++ frontend/client/src/chat.jsx
@@ -1,5 +1,22 @@
 export default function Chat () {
-    return(
-        <di>chat here</di>
-    );
+  return(
+    <div className="flex h-screen">
+          <div className="bg-blue-100 w-1/3">
+            contacs
+          </div>
+          <div className="bg-blue-300 w-2/3">
+            <div>messages with selected person</div>
+            <div>
+              <input type="tex"
+                     placeholder="Type your messege here" 
+                     className="bg-white borer p-2"/>
+              <button>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M6 12L3.269 3.126A59.768 59.768 0 0121.485 12 59.77 59.77 0 013.27 20.876L5.999 12zm0 0h7.5" />
+</svg>
+              </button>
+            </div>
+          </div>
+        </div>
+    ); 
 }
