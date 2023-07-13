c97692bad53755cd99528f1e673123b865cad5f9
tonyxde <anthonybeitia2@gmail.com>
2023-07-12 18:04:31 -0600

    Datos de inscripcion 3

--- frontend/client/src/Register.jsx
+++ frontend/client/src/Register.jsx
@@ -16,14 +16,18 @@ export default function Register() {
         <div className="bg-blue-50 h-screen flex items-center">
             <form className="w-64 mx-auto m-12" onSubmit={register}> 
                 <input value={username}
-                 onChange={ev => setUsername(ev.target.value)}
+                  onChange={ev => setUsername(ev.target.value)}
                   type="text" placeholder="username"
                   className="block w-full rounded-sm p-2 mb-2 border"/>
                 <input value={password}
-                 onChange={ev => setPassword(ev.target.value)} type="password"
+                 onChange={ev => setPassword(ev.target.value)} 
+                  type="password"
                   placeholder="password"
-                    className="block w-full rounded-sm p-2 mb-2 border"/>
+                  className="block w-full rounded-sm p-2 mb-2 border"/>
                 <button className="bg-blue-500 tex-white block w-full rounded-sm p-2">Register</button>
+                <div className="text-center mt-2">
+                  Already a member? <a href="">Login here</a>
+                </div>
             </form>
         </div>
     );
